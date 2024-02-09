c36897af85f3716a6d123d210980c3ac8ca64359
satish <mauryasatish0007@gmail.com>
2024-02-09 19:50:24 +0530

    add the commas and code formatting add the const keywords whereever is possible

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,60 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: Column(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
           children: [
-            Center(
+            const Center(
               child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: AssetImage('assets/images/prabodham2.png')),
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'assets/images/prabodham2.png',
+                ),
+              ),
             ),
-            SizedBox(height: 50),
+            const SizedBox(
+              height: 50,
+            ),
             Row(
               children: [
-                SizedBox(
+                const SizedBox(
                   width: 60,
                 ),
                 TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Login',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    )),
-                SizedBox(
+                  onPressed: () {},
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                const SizedBox(
                   width: 110,
                 ),
                 Padding(
                   padding: const EdgeInsets.only(right: 40),
                   child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, color: Colors.black),
-                      )),
+                    onPressed: () {},
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -50,99 +63,135 @@ class MyApp extends StatelessWidget {
                   left: 20, right: 20, top: 20, bottom: 10),
               child: TextField(
                 decoration: InputDecoration(
-                    hintText: 'Username or Mobile Number',
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0)),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 5),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Phone No.',
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0)),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 5),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0)),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 5),
-              child: TextField(
-                decoration: InputDecoration(
-                    hintText: 'Confirm Password',
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0)),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.only(left: 20, right: 20, top: 8, bottom: 5),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Referal Code(optional)',
+                  hintText: 'Username or Mobile Number',
                   filled: true,
                   fillColor: Colors.grey[200],
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10.0),
                     borderSide: BorderSide.none,
                   ),
-                  contentPadding: EdgeInsets.symmetric(
+                  contentPadding: const EdgeInsets.symmetric(
                     vertical: 15.0,
                     horizontal: 20.0,
                   ),
                 ),
               ),
             ),
-            SizedBox(
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 8,
+                bottom: 5,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Phone No.',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                    horizontal: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 8,
+                bottom: 5,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                    horizontal: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 8,
+                bottom: 5,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Confirm Password',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      10.0,
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                    horizontal: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                right: 20,
+                top: 8,
+                bottom: 5,
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Referal Code(optional)',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      10.0,
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    vertical: 15.0,
+                    horizontal: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
               height: 20,
             ),
-            Container(
-                width: 300,
-                child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red)),
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(color: Colors.white),
-                    )))
+            SizedBox(
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                ),
+                child: const Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
           ],
-        )));
+        ),
+      ),
+    );
   }
 }
